853af0f30edcb9152faca0e6ba70c963ea153f08
xbouchlis <x.bouchlis@atc.gr>
2018-01-17 17:36:15 +0200

    get sentiment analysis for given term

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var Twitter = require('twitter');
+var SentimentAnalysis = require('sentiment-analysis');
 require('dotenv').config();
-
-var status = `
-Binance is TEMPORARILY accepting new users! HURRY! 💰💸
-
-▶️ binance.com/?ref=10880218 ◀️
-
-Thank me later 😄
-
-$NAS $ETH $RDD $NEO $ETC $GXS $REP $WTC $XLM $ARK $ICX $DOGE $QASH $BTC $ENG $GNT $SC $ZCL
-`
 
 var client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -17,26 +8,22 @@ var client = new Twitter({
   access_token_key: process.env.TOKEN_KEY,
   access_token_secret: process.env.TOKEN_SECRET,
 });
- 
-var params = {screen_name: 'nodejs'};
-// TEST
-//printTimeline(params)
 
-function tweet(status){
-	client.post('statuses/update', {status: status}, function(error, tweet, response) {
-		if (!error) {
-			console.log(tweet);
-		}
-	});
+function searchTweets(params, callback){
+	client.get('search/tweets', {q: params.query}, function(error, tweets, response) {
+		callback(tweets.statuses);
+	});	
 }
 
-function printTimeline(params){
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		tweets.forEach(function(t){
-			console.log(t.text);
-		});
-	});
-}
-
-tweet('test tweet');
-
+// Sentiment analysis for term: bitcoin
+searchTweets({query: '$XLM'}, function(tweets){
+	var score = 0;
+	tweets.forEach(function(t){
+		var tweetScore = SentimentAnalysis(t.text);
+		console.log(tweetScore + "\t" + t.text + "\n" + "__________________________");
+		score += tweetScore;
+	})
+	console.log("********************************");
+	console.log("Tweets analyzed:", tweets.length);
+	console.log("Sentiment score:", score);
+});
