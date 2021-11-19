9bf0875a12deca3d61db59e2a562aa09b245f955
zkbitcoin <github@zkbitcoin.com>
2021-11-19 08:24:19 -0500

    update

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 export PATH="$(yarn global bin):$PATH"
 
-serve -l 4000 --ssl-cert /home/pivx/cert/blockbook.crt --ssl-key /home/pivx/cert/blockbook.key -n -s /home/pivx/go/src/blockbook/binance/build > run.out 2>&1  </dev/null &
+#serve -l 4000 --ssl-cert /home/pivx/cert/blockbook.crt --ssl-key /home/pivx/cert/blockbook.key -n -s /home/pivx/go/src/blockbook/binance/build > run.out 2>&1  </dev/null &
+
+yarn start 2>&1  </dev/null &
